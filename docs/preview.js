192e762d307c43251ebfceb01397fea0e5ea82f8
Daniel Losada <daniel.losada@ehu.eus>
2025-01-18 20:19:23 +0100

    1.1

--- docs/preview.js
+++ docs/preview.js
@@ -19,69 +19,58 @@ function generarImpresion() {
     const fechaActualLargaEuskara = new Date().toLocaleDateString('eu', { year: 'numeric', month: 'long', day: 'numeric' });
 
     contenidoParaPDF = `
-         <link rel="stylesheet" href="style.css">
-         <div style="display: flex; align-items: center; justify-content: center; gap: 20px;">
-        <img src="https://www.ehu.eus/documents/2947353/2967241/blanco_pequeno.jpg" alt="Logo UPV/EHU" style="width: 10%;">
-        <h1>Tutoretza Ordutegiaren Aldaketa / Formulario de Cambio de Tutorías</h1>
-    </div>
-        <div>
-            <div class="output">
-             <h1>Tutoretza Ordutegiaren Aldaketa / Formulario de Cambio de Tutorías</h1>
-             <br>
-                <h3>Argitaratzeko kopia / Ejemplar para hacer público</h3>
-                <p><strong>Irakaslea<br>Profesor:</strong> ${profesor}</p>
-              <div class="column-container">
-    <div class="column">
-        <h4>Aldatutako Data eta Ordua<br>Fecha y Hora que se Modifica</h4>
-        <p>Data / Fecha: ${fechaModifica}</p>
-        <p>Ordua / Hora: ${horaModificaInicio} - ${horaModificaFin}</p>
-    </div>
-    <div class="column">
-        <h4>Ordutegi Berria<br>Nuevo Horario</h4>
-        <p>Data / Fecha: ${fechaNueva}</p>
-        <p>Ordua / Hora: ${horaNuevaInicio} - ${horaNuevaFin}</p>
-    </div>
-</div>
-                <center>
-                    <p>
-                        Donostian, ${fechaActualLargaEuskara}
-                        <br>
-                        En Donostia-San Sebastián, ${fechaActualLarga}
-                    </p>
-                    <br>
-                    <p>Firma y Sello</p>
-                </center>
-            </div>
-        </div>
-        <div>
+        <link rel="stylesheet" href="style.css">
+        
         <div style="display: flex; align-items: center; justify-content: center; gap: 20px;">
-        <img src="https://www.ehu.eus/documents/2947353/2967241/blanco_pequeno.jpg" alt="Logo UPV/EHU" style="width: 10%;">
-        <h1>Tutoretza Ordutegiaren Aldaketa / Formulario de Cambio de Tutorías</h1>
-    </div>
-            <div class="output">
-                         <br>
-                <h3>ikastegi edo sailarako kopia / Ejemplar para el centro/departamento</h3>
-                <p><strong>Irakaslea<br>Profesor:</strong> ${profesor}</p>
-                <div class="column-container">
-    <div class="column">
-        <h4>Aldatutako Data eta Ordua<br>Fecha y Hora que se Modifica</h4>
-        <p>Data / Fecha: ${fechaModifica}</p>
-        <p>Ordua / Hora: ${horaModificaInicio} - ${horaModificaFin}</p>
-    </div>
-    <div class="column">
-        <h4>Ordutegi Berria<br>Nuevo Horario</h4>
-        <p>Data / Fecha: ${fechaNueva}</p>
-        <p>Ordua / Hora: ${horaNuevaInicio} - ${horaNuevaFin}</p>
-    </div>
-</div>
-                <center>
-                    <p>
-                        Donostian, ${fechaActualLargaEuskara}
-                        <br>
-                        En Donostia-San Sebastián, ${fechaActualLarga}
-                    </p>
-                </center>
+            <img src="https://www.ehu.eus/documents/2947353/2967241/blanco_pequeno.jpg" alt="Logo UPV/EHU" style="width: 10%;">
+            <h1>Tutoretza Ordutegiaren Aldaketa / Formulario de Cambio de Tutorías</h1>
+            <br>
+        </div>
+
+        <div class="output"> 
+            <h3>Argitaratzeko kopia / Ejemplar para hacer público</h3>
+            <p><strong>Irakaslea<br>Profesor:</strong> ${profesor}</p>
+            
+            <div class="column-container">
+                <div class="column">
+                    <h4>Aldatutako Data eta Ordua<br>Fecha y Hora que se Modifica</h4>
+                    <p>Data / Fecha: ${fechaModifica}</p>
+                    <p>Ordua / Hora: ${horaModificaInicio} - ${horaModificaFin}</p>
+                </div>
+            
+                <div class="column">
+                    <h4>Ordutegi Berria<br>Nuevo Horario</h4>
+                    <p>Data / Fecha: ${fechaNueva}</p>
+                    <p>Ordua / Hora: ${horaNuevaInicio} - ${horaNuevaFin}</p>
+                </div>
             </div>
+                <center><p>Donostian, ${fechaActualLargaEuskara}<br>En Donostia-San Sebastián, ${fechaActualLarga}</p></center>
+        </div>
+
+        <div style="display: flex; align-items: center; justify-content: center; gap: 20px;">
+            <img src="https://www.ehu.eus/documents/2947353/2967241/blanco_pequeno.jpg" alt="Logo UPV/EHU" style="width: 10%;">
+            <h1>Tutoretza Ordutegiaren Aldaketa / Formulario de Cambio de Tutorías</h1>
+            <br>
+        </div>
+
+        <div class="output">
+            <h3>ikastegi edo sailarako kopia / Ejemplar para el centro/departamento</h3>
+            <p><strong>Irakaslea<br>Profesor:</strong> ${profesor}</p>
+        
+            <div class="column-container">
+                <div class="column">
+                    <h4>Aldatutako Data eta Ordua<br>Fecha y Hora que se Modifica</h4>
+                    <p>Data / Fecha: ${fechaModifica}</p>
+                    <p>Ordua / Hora: ${horaModificaInicio} - ${horaModificaFin}</p>
+                </div>
+            
+                <div class="column">
+                    <h4>Ordutegi Berria<br>Nuevo Horario</h4>
+                    <p>Data / Fecha: ${fechaNueva}</p>
+                    <p>Ordua / Hora: ${horaNuevaInicio} - ${horaNuevaFin}</p><br>
+                </div>
+            </div>
+                <center><p>Donostian, ${fechaActualLargaEuskara}<br>En Donostia-San Sebastián, ${fechaActualLarga}</p></center>
         </div>
     `;
 
