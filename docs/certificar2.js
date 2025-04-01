1d80071825c9e6b16cd31f18d9abaea1c59a1d93
Daniel Losada <daniel.losada@ehu.eus>
2025-04-01 08:57:35 +0200

    Update certificar2.js

--- docs/certificar2.js
+++ docs/certificar2.js
@@ -1,3 +1,23 @@
+function seleccionarArchivoCertificado() {
+    return new Promise((resolve, reject) => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.p12,.pfx';
+        
+        input.onchange = (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                resolve(file);
+            } else {
+                reject(new Error('No se seleccionó ningún archivo'));
+            }
+        };
+        
+        input.click();
+    });
+}
+
+
 // Función para seleccionar el archivo de certificado
 function seleccionarArchivoCertificado() {
     return new Promise((resolve, reject) => {
